a54186d96ca0ce807a8997aa4c69b44a79ad0e90
Mark Olliver <mark@olliver.me.uk>
2023-09-11 10:50:19 +0100

    actually remove the calls

--- utils/init.go
+++ utils/init.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/joho/godotenv"
 	"golang.org/x/exp/slog"
 )
 
@@ -17,17 +16,12 @@ var (
 	Logger      *slog.Logger                   // Logger is the default logger
 	LogLevel    string                         // logLevel is the log level
 	Err         error                          // Err is the default error
-	Hostname, _ = os.Hostname()                // Hostname is the default hostname
 	Environment = GetEnv("ENVIRONMENT", "dev") // Environment is the default environment
 	Prefix      = GetEnv("PREFIX", "data")     // Prefix is the default prefix
 )
 
 // init loads the .env file and sets the log level
 func init() {
-	// loads values from .env into the system
-	if err := godotenv.Load(); err != nil {
-		slog.Debug("No .env file found")
-	}
 	LogLevel = GetEnv("LOG_LEVEL", "INFO")
 	Logger = initLog()
 }
